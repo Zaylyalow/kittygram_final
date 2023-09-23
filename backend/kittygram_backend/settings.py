4667aac5c2fbae481977c1ad8ff000f74fcb783e
Shamil Zaylyalov <Zaylyalow@gmail.com>
2023-09-23 15:34:23 +0500

    fix static

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.72.191', '127.0.0.1', 'localhost', 'kittygramufa.ddns.net']
 INSTALLED_APPS = [
